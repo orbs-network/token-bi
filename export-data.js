7966c763a31e671a9fc65cc2f4f8b0e56da5f488
Jonathan Levison <jonathan.levison@gmail.com>
2019-05-13 13:12:14 +0300

    small refactor, csv files to gitignore

--- export-data.js
+++ export-data.js
@@ -7,19 +7,21 @@ var ProgressBar = require('progress');
 let ethereumConnectionURL = "https://mainnet.infura.io/v3/6e3487b19a364da6965ca35a72fb6d68"; //infura endpoint
 let erc20ContractAddress = "0xff56Cc6b1E6dEd347aA0B7676C85AB0B3D08B0FA"; //token contract
 let votingContractAddress = "0x30f855afb78758Aa4C2dc706fb0fA3A98c865d2d"; //voting
-let guardiansContractAddress = "0xD64B1BF6fCAb5ADD75041C89F61816c2B3d5E711"; //guardians
-let startBlock = "7437000";//transactions start at 7437000; //contract created at 5710114 
-//let endBlock = "7669000";//"7590000"; //_latest_ , first election at 7528900
-//let endBlock = "7452866";//should be first 97 transfers - excluding mint+wallet change (so 99 total if start is "0")
-let endBlock = "7470000";
-let interval = 20000;
-let doTransfers = false;
+let guardiansContractAddress = "0xD64B1BF6fCAb5ADD75041C89F61816c2B3d5E711"; //guardians - not used yet
+let startBlock = "7669000";//transactions start at 7437000; //contract created at 5710114 
+let endBlock = "7748900"; // last elections as of now.. first election at 7528900
+let interval = 10000;
+let doTransfers = true;
 let doDelegates = true;
 let transfers_filename = "transfers.csv"; 
 let delegate_filename = "delegates.csv";
 let withHumanDate = false;
 const TOKEN_ABI = [{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}];
 const VOTING_ABI = [{"anonymous":false,"inputs":[{"indexed":true,"name":"voter","type":"address"},{"indexed":false,"name":"validators","type":"address[]"},{"indexed":false,"name":"voteCounter","type":"uint256"}],"name":"VoteOut","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"delegator","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"delegationCounter","type":"uint256"}],"name":"Delegate","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"delegator","type":"address"},{"indexed":false,"name":"delegationCounter","type":"uint256"}],"name":"Undelegate","type":"event"},{"constant":false,"inputs":[{"name":"validators","type":"address[]"}],"name":"voteOut","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"}],"name":"delegate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"undelegate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"guardian","type":"address"}],"name":"getCurrentVote","outputs":[{"name":"validators","type":"address[]"},{"name":"blockNumber","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"guardian","type":"address"}],"name":"getCurrentVoteBytes20","outputs":[{"name":"validatorsBytes20","type":"bytes20[]"},{"name":"blockNumber","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"delegator","type":"address"}],"name":"getCurrentDelegation","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}];
+const TRANSFERS_HEADER = "From,To,Amount,txnIndex,txnHash,Block,UnixDate";
+const DELEGATES_HEADER = "From,To,txnIndex,txnHash,Block,UnixDate";
+const TRANSFER_EVENT_NAME = "Transfer";
+const DELEGATE_EVENT_NAME = "Delegate";
 
 
 function validateInput() {
@@ -161,57 +163,40 @@ async function readAndMergeEvents(web3, contract, startBlock, endBlock, eventNam
     return events;
 }
 
-async function getTransferEvents(web3, tokenContract) {
-    let transfers = await readAndMergeEvents(web3, tokenContract, startBlock, endBlock, 'Transfer', false);
-    console.log('\x1b[33m%s\x1b[0m', `Merged to ${transfers.length} Transfer events`);
-
-    let csvStr = 'From,To,Amount,txnIndex,txnHash,Block,UnixDate\n';
+function getHumanDateForRow(row) {
+    let humanDatePart = "";
     if (withHumanDate) {
-        csvStr = 'From,To,Amount,txnIndex,txnHash,Block,UnixDate,HumanDate\n';
+        humanDatePart = `,${row.human_date}`;
     }
-
-    for (let i = 0;i < transfers.length;i++) {
-        let result = transfers[i];
-        // too verbose..
-        // console.log('%s \x1b[34m%s\x1b[0m %s \x1b[34m%s\x1b[0m %s \x1b[35m%s\x1b[0m %s \x1b[36m%s\x1b[0m',
-        //     `From`, `${result.transferFrom}`,
-        //     `To`, `${result.transferTo}`,
-        //     `amount`, `${result.amount}`,
-        //     `at block`, `${result.block}`);
-        let humanDatePart = ""
-        if (withHumanDate) {
-            humanDatePart = `,${result.human_date}`
-        }
-
-        csvStr += `${result.transferFrom},${result.transferTo},${result.amount},${result.transactionIndex},${result.txHash},${result.block},${result.unix_date}${humanDatePart}\n`;
-    }
-
-    fs.writeFileSync(transfers_filename, csvStr);
-    console.log('\x1b[33m%s\x1b[0m', `Transfers CSV version file was saved to ${transfers_filename}!`);
+    return humanDatePart;
 }
 
-async function getDelegateEvents(web3, votingContract) {
-    let delegates = await readAndMergeEvents(web3, votingContract, startBlock, endBlock, 'Delegate', false);
-    console.log('\x1b[33m%s\x1b[0m', `Merged to ${delegates.length} Delegate events`);
+function formatDelegate(row) {
+    let humanDatePart = getHumanDateForRow(row);
+    return `${row.transferFrom},${row.transferTo},${row.transactionIndex},${row.txHash},${row.block},${row.unix_date}${humanDatePart}\n`;
+}
 
-    let csvStr = 'From,To,txnIndex,txnHash,Block,UnixDate\n';
+function formatTransfer(row) {
+    let humanDatePart = getHumanDateForRow(row);
+    return `${row.transferFrom},${row.transferTo},${row.amount},${row.transactionIndex},${row.txHash},${row.block},${row.unix_date}${humanDatePart}\n`;
+}
+
+async function getEvents(web3, contract, eventName, csvHeader, appendFunc, outputFilename) {
+    let eventsData = await readAndMergeEvents(web3, contract, startBlock, endBlock, eventName, false);
+    console.log('\x1b[33m%s\x1b[0m', `Merged to ${eventsData.length} Delegate events`);
+
+    let csvStr = csvHeader + "\n";
     if (withHumanDate) {
-        csvStr = 'From,To,txnIndex,txnHash,Block,UnixDate,HumanDate\n';
+        csvStr = csvHeader + ',HumanDate\n';
+    } 
+
+    for (let i = 0;i < eventsData.length;i++) {
+        let row = eventsData[i];
+        csvStr += appendFunc(row);
     }
 
-    for (let i = 0;i < delegates.length;i++) {
-        let result = delegates[i];
-
-        let humanDatePart = ""
-        if (withHumanDate) {
-            humanDatePart = `,${result.human_date}`
-        }
-
-        csvStr += `${result.transferFrom},${result.transferTo},${result.transactionIndex},${result.txHash},${result.block},${result.unix_date}${humanDatePart}\n`;
-    }
-
-    fs.writeFileSync(delegate_filename, csvStr);
-    console.log('\x1b[33m%s\x1b[0m', `Delegate CSV version file was saved to ${delegate_filename}!`);
+    fs.writeFileSync(outputFilename, csvStr);
+    console.log('\x1b[33m%s\x1b[0m', `${eventName} CSV version file was saved to ${outputFilename}!`);
 }
 
 async function main() {
@@ -219,12 +204,12 @@ async function main() {
     let web3 = await new Web3(new Web3.providers.HttpProvider(ethereumConnectionURL));
     if (doTransfers) {
         let tokenContract = await new web3.eth.Contract(TOKEN_ABI, erc20ContractAddress);
-        await getTransferEvents(web3, tokenContract);
+        await getEvents(web3, tokenContract, TRANSFER_EVENT_NAME, TRANSFERS_HEADER, formatTransfer, transfers_filename);
     }
 
     if (doDelegates) {
         let votingContract = await new web3.eth.Contract(VOTING_ABI, votingContractAddress);
-        await getDelegateEvents(web3, votingContract);
+        await getEvents(web3, votingContract, DELEGATE_EVENT_NAME, DELEGATES_HEADER, formatDelegate, delegate_filename);
     }
 }
 
