df1ddea287cbadda6fc72333e399d45f74991899
Or Lavy <orlavyil@gmail.com>
2020-03-05 18:02:01 +0200

    Moves all of the paramteres to configs

--- export-data.ts
+++ export-data.ts
@@ -9,42 +9,27 @@ import {
 } from './src/csv/ecentsDataToCsv';
 import {getEvents} from './src/erc20Events/erc20Events';
 import { convertEventDataToCsvRowForm } from './src/orbsEvents/orbsEventConverter'
-import {ELECTION_BLOCKS} from './src/electionBlocks';
 import {ABIS, CONTRACTS, CSV_CONSTANTS, EVENT_NAMES} from './scriptConstants';
-
-let START_BLOCK = ELECTION_BLOCKS['104'].ethereumBlockNumber + 1; //transactions start at 7437000; //contract created at 5710114
-let END_BLOCK = ELECTION_BLOCKS['105'].ethereumBlockNumber; // last elections as of now.. first election at 7528900
-const INTERVAL = 200000;
-
-const doTransfers = true;
-const doDelegates = true;
-const doGuardians = true;
-const doVotes = true;
-
-const filenameVoteOut = "votes.csv";
-let filenameTransfers = "transfers.csv";
-let filenameDelegates = "delegates.csv";
-const filenameGuardiansRegister = "guardian_register.csv";
-const filenameGuardiansLeave = "guardian_leave.csv";
-const withHumanDate = false;
+import {configs} from './scriptConfigs';
+const { outputPaths, activationFlags, outputFlags, blocksReading } = configs;
 
 async function main(startBlock: number, endBlock: number,  eventBatchingSize: number,
                     executionFlags: { doTransfers: boolean, doDelegates: boolean, doGuardians: boolean, doVotes: boolean },
                     outputFlags: { addHumanReadableDate: boolean }) {
-    const web3 = await new Web3(new Web3.providers.HttpProvider(ethereumConnectionURL));
+    const web3 = await new Web3(new Web3.providers.HttpProvider(configs.ethereumConfigs.ethereumConnectionURL));
 
     if (executionFlags.doTransfers) {
         const tokenContract = await new web3.eth.Contract(ABIS.token, CONTRACTS.erc20ContractAddress);
         const transferEvents = await getEvents(web3, tokenContract, EVENT_NAMES.transfer, startBlock, endBlock, eventBatchingSize, convertEventDataToCsvRowForm);
 
-        await writeEventsDataToCsv(transferEvents, CSV_CONSTANTS.transfersHeader, EVENT_NAMES.transfer, filenameTransfers, formatTransfer, outputFlags.addHumanReadableDate);
+        await writeEventsDataToCsv(transferEvents, CSV_CONSTANTS.transfersHeader, EVENT_NAMES.transfer, outputPaths.transfers, formatTransfer, outputFlags.addHumanReadableDate);
     }
 
     if (executionFlags.doDelegates) {
         const votingContract = await new web3.eth.Contract(ABIS.voting, CONTRACTS.votingContractAddress);
 
         const delegatesEvents = await getEvents(web3, votingContract, EVENT_NAMES.delegate, startBlock, endBlock, eventBatchingSize, convertEventDataToCsvRowForm);
-        await writeEventsDataToCsv(delegatesEvents, CSV_CONSTANTS.delegatesHeader, EVENT_NAMES.delegate, filenameDelegates, formatDelegate, outputFlags.addHumanReadableDate);
+        await writeEventsDataToCsv(delegatesEvents, CSV_CONSTANTS.delegatesHeader, EVENT_NAMES.delegate, outputPaths.delegates, formatDelegate, outputFlags.addHumanReadableDate);
     }
 
     if (executionFlags.doGuardians) {
@@ -52,29 +37,29 @@ async function main(startBlock: number, endBlock: number,  eventBatchingSize: nu
 
         // Guardians register
         const guardianRegisterEvents = await getEvents(web3, guardianContract, EVENT_NAMES.guardianRegister, startBlock, endBlock, eventBatchingSize, convertEventDataToCsvRowForm);
-        await writeEventsDataToCsv(guardianRegisterEvents, CSV_CONSTANTS.guardiansHeader, EVENT_NAMES.guardianRegister, filenameGuardiansRegister, formatGuardian, outputFlags.addHumanReadableDate);
+        await writeEventsDataToCsv(guardianRegisterEvents, CSV_CONSTANTS.guardiansHeader, EVENT_NAMES.guardianRegister, outputPaths.guardiansRegister, formatGuardian, outputFlags.addHumanReadableDate);
 
         // Guardians leave
         const guardianLeaveEvents = await getEvents(web3, guardianContract, EVENT_NAMES.guardianLeave, startBlock, endBlock, eventBatchingSize, convertEventDataToCsvRowForm);
-        await writeEventsDataToCsv(guardianLeaveEvents, CSV_CONSTANTS.guardiansHeader, EVENT_NAMES.guardianLeave, filenameGuardiansLeave, formatGuardian, outputFlags.addHumanReadableDate);
+        await writeEventsDataToCsv(guardianLeaveEvents, CSV_CONSTANTS.guardiansHeader, EVENT_NAMES.guardianLeave, outputPaths.guardiansLeave, formatGuardian, outputFlags.addHumanReadableDate);
     }
 
     if (executionFlags.doVotes) {
         const votingContract = await new web3.eth.Contract(ABIS.voting, CONTRACTS.votingContractAddress);
 
         const voteoutEvents = await getEvents(web3, votingContract, EVENT_NAMES.voteOut, startBlock, endBlock, eventBatchingSize, convertEventDataToCsvRowForm);
-        await writeEventsDataToCsv(voteoutEvents, CSV_CONSTANTS.voteOutHeader, EVENT_NAMES.voteOut, filenameVoteOut, formatVoteOut, outputFlags.addHumanReadableDate);
+        await writeEventsDataToCsv(voteoutEvents, CSV_CONSTANTS.voteOutHeader, EVENT_NAMES.voteOut, outputPaths.voteOut, formatVoteOut, outputFlags.addHumanReadableDate);
     }
 }
 
-main(START_BLOCK, END_BLOCK, INTERVAL,
+main(blocksReading.startBlock, blocksReading.endBlock, blocksReading.blocksInterval,
     {
-        doTransfers,
-        doDelegates,
-        doGuardians,
-        doVotes
+        doTransfers: activationFlags.processTransfers,
+        doDelegates: activationFlags.processDelegates,
+        doGuardians: activationFlags.processGuardians,
+        doVotes: activationFlags.processVotes,
     }, {
-        addHumanReadableDate: withHumanDate,
+        addHumanReadableDate: outputFlags.withHumanDate,
     })
     .then(results => {
         console.log('\x1b[33m%s\x1b[0m', "\n\nDone!!\n");
--- scriptConfigs.ts
+++ scriptConfigs.ts
@@ -1,11 +1,11 @@
-import {AbiItem} from 'web3-utils';
-
 //let ethereumConnectionURL = "http://ec2-18-222-114-71.us-east-2.compute.amazonaws.com:8545"; //orbs endpoint
+import {ELECTION_BLOCKS} from './src/electionBlocks';
+
 const ethereumConnectionURL = "https://mainnet.infura.io/v3/6e3487b19a364da6965ca35a72fb6d68"; //infura endpoint
 
-let startBlock = 8408900; //transactions start at 7437000; //contract created at 5710114
-let endBlock = 8548900; // last elections as of now.. first election at 7528900
-const blocksInterval = 100000;
+let startBlock = ELECTION_BLOCKS['104'].ethereumBlockNumber + 1; //transactions start at 7437000; //contract created at 5710114
+let endBlock = ELECTION_BLOCKS['105'].ethereumBlockNumber; // last elections as of now.. first election at 7528900
+const blocksInterval = 200000;
 const processTransfers = true;
 const processDelegates = true;
 const processGuardians = true;
