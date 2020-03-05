8c57f63550ce707322d36a76979c60d05d08f943
Or Lavy <orlavyil@gmail.com>
2020-03-05 17:52:38 +0200

    Moves the scripts to their own folder

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -2,3 +2,5 @@
 mv delegates.csv delegates-$1-$2.csv
 mv transfers.csv transfers-$1-$2.csv
 mv votes.csv votes-$1-$2.csv
+mv guardian_leave.csv guardian_leave-$1-$2.csv
+mv guardian_register.csv guardian_register-$1-$2.csv
