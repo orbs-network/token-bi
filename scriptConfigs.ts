4ec9c5caad519c5886a3372c9719f2a5f098c9ef
Or Lavy <orlavyil@gmail.com>
2020-05-19 16:19:27 +0300

    Improves generating of elections matching block numbers

--- scriptConfigs.ts
+++ scriptConfigs.ts
@@ -1,10 +1,13 @@
 //let ethereumConnectionURL = "http://ec2-18-222-114-71.us-east-2.compute.amazonaws.com:8545"; //orbs endpoint
 import {ELECTION_BLOCKS} from './src/electionBlocks';
+import ELECTION_BLOCKS_JSON from './src/electionBlocks.json';
 
 const ethereumConnectionURL = "https://mainnet.infura.io/v3/6e3487b19a364da6965ca35a72fb6d68"; //infura endpoint
 
-let startBlock = ELECTION_BLOCKS['107'].firstBlock; //transactions start at 7437000; //contract created at 5710114
-let endBlock = ELECTION_BLOCKS['110'].lastBlock; // last elections as of now.. first election at 7528900
+// let startBlock = ELECTION_BLOCKS['107'].firstBlock; //transactions start at 7437000; //contract created at 5710114
+// let endBlock = ELECTION_BLOCKS['110'].lastBlock; // last elections as of now.. first election at 7528900
+const startBlock = ELECTION_BLOCKS_JSON["111"].firstBlock; //transactions start at 7437000; //contract created at 5710114
+const endBlock = ELECTION_BLOCKS_JSON["128"].lastBlock; // last elections as of now.. first election at 7528900
 const blocksInterval = 200000;
 const processTransfers = true;
 const processDelegates = true;
---
+++ src/generateElectionsLastBLockNumbers.ts
@@ -0,0 +1,34 @@
+import fs from 'fs';
+
+const earliestElection = 103;
+const earliestElectionLastBlockBlock = 9568900;
+const INTERVAL_BETWEEN_ELECTIONS = 20_000;
+
+function generateBlocks(upUntilElection: number) {
+  if (upUntilElection <= earliestElection) {
+    throw new Error(`Invalid election number of ${upUntilElection}, minimum should be ${earliestElection + 1}`);
+  }
+
+  let currentElection = earliestElection + 1;
+  let lastBlockIncluded = earliestElectionLastBlockBlock;
+
+  const ELECTIONS = {};
+
+  while (currentElection <= upUntilElection) {
+    const lastBLockForCurrentElection = lastBlockIncluded + INTERVAL_BETWEEN_ELECTIONS;
+
+    ELECTIONS[currentElection] = {
+      firstBlock: lastBlockIncluded + 1,
+      lastBlock: lastBLockForCurrentElection,
+    };
+
+    currentElection++;
+    lastBlockIncluded = lastBLockForCurrentElection;
+  }
+
+  console.log(ELECTIONS);
+
+  fs.writeFileSync('./src/electionBlocks.json', JSON.stringify(ELECTIONS, null, 2));
+}
+
+generateBlocks(128);
