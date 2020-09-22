fcd5d4a9a84a572cfc1d03465030bb578cbd66cc
Or Lavy <orlavyil@gmail.com>
2020-09-22 17:12:00 +0200

    Adds unique address parser

---
+++ src/directRewards/extractUniqueAddresses.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+const parse = require('csv-parse/lib/sync')
+
+const FILE_NAME = './staked_events_1_171.csv'
+const STAKER_ADDRESS_INDEX = 0;
+
+async function main() {
+    const rawFileText = fs.readFileSync(FILE_NAME).toString();
+    const lines = parse(rawFileText);
+    const  [headerLine, ...dataLines] = lines;
+
+    const onlyAddresses = dataLines.map(line => line[STAKER_ADDRESS_INDEX]);
+    const addressesSet = new Set(onlyAddresses);
+    const uniqueAddresses = Array.from(addressesSet);
+
+    console.log(`Found ${uniqueAddresses.length} unique addresses with staking`);
+
+    fs.writeFileSync('./uniqueAddresses.json', JSON.stringify(uniqueAddresses, null, 2));
+}
+
+main();
